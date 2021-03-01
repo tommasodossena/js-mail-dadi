723e1b9d6510a73ae6204a1b561ca42aa94ba5ef
Tommaso <dossena.tommaso.dt@gmail.com>
2021-03-01 16:50:19 +0100

    Access list // Working

--- js/script.js
+++ js/script.js
@@ -1,4 +1,34 @@
-/* 
+/*
+ESERIZIO 1: ACCESS LIST
+Chiedi all’utente la sua email,
+controlla che sia nella lista di chi può accedere,
+stampa un messaggio appropriato sull’esito del controllo.
+*/
+
+// 1 - Lista mail con permessi
+var list = ["franco@mail.com", "pippo@mail.com", "pierino@mail.com", "mario@mail.com", "luigi@mail.com", "marcello@mail.com"];
+
+// 2 - Chiedere all'utente di inserire la propria mail
+var mail = prompt("inserisci la tua mail");
+
+// 3 - Valore di default della verifica 
+var check = false;
+
+// 4 - Verificare se l'utente può accedere
+for (var i = 0; i < list.length; i++) {
+    if (mail === list[i]) {
+        check = true;
+    }
+}
+
+// 5 - Stampare a schermo l'esito del controllo
+if (check) {
+    alert("Indirizzo verificato, puoi accedere alla pagina");
+} else {
+    alert("Accesso negato, non puoi accedere alla pagina");
+}
+
+/*
 ESERIZIO 2: DADI
 Generare un numero random da 1  a 6, sia per il giocatore sia per il computer 
 Stabilire il vincitore, in base a chi fa il punteggio più alto. 
@@ -17,8 +47,6 @@ if (giocatore > computer) {
     alert("Congratulazioni! Il punteggio del tuo dado (" + giocatore + ") è superiore al punteggio ottenuto dal computer (" + computer + ").");
 } else if (giocatore == computer) {
     alert("Patta, riprova! Il tuo dado e quello del computer hanno ottenuto lo stesso punteggio (" + giocatore + ").");
-    location.reload()
 } else {
     alert("Hai perso, riprova! Il punteggio ottenuto dal computer (" + computer + ") è superiore al punteggio del tuo dado (" + giocatore + ").");
-    location.reload()
 }
